afb18b648e2cd4de0235a3b9ad2a4d7fc27442ff
AlanPratama <109650474+AlanPratama@users.noreply.github.com>
2023-10-27 21:09:01 +0700

    Update game.js

--- game/monarch-flappy-plane-master/content/js/game.js
+++ game/monarch-flappy-plane-master/content/js/game.js
@@ -16,7 +16,8 @@ $(document).ready(() => {
     }
 
     $("#show").click(() => { location.reload(); });
-
+    if ($(this).mousedown()) {
+        
     $(this).mousedown(() => {
         if (!started) {
             started = true;
@@ -69,9 +70,75 @@ $(document).ready(() => {
 
         depressed = true;
     });
+} 
 
+if($(this).touchstart()){
+    $(this).touchstart(() => {
+        if (!started) {
+            started = true;
+            if (event == undefined) {
+                let max = parseInt($(document).height());
+                let top = parseInt(plane.css("top"));
+
+                $("#show").hide();
+
+                event = setInterval(() => {
+                    ticks++;
+                    santa++;
+
+                    plane.css({
+                        "top": (top = depressed ? (top < 41) ? 40 : top - 3 : top + 3),
+                        "transform": depressed ? "rotate(-10deg)" : "rotate(10deg)"
+                    });
+
+                    $(".santa").each(function() {
+                        const sl = $(this).position().left;
+                        const st = $(this).position().top;
+                        const pl = plane.position().left;
+                        const pt = plane.position().top;
+
+                        if(sl < 0) {
+                            $(this).remove();
+                            $("#score").text(score++);
+                        } else {
+                            $(this).css("left", sl - $(this).data("rate"));
+                        }
+
+                        if(sl > pl && sl < pl + 168 && st > pt && st < pt + 80) {
+                            overlay("explode", "KAMU MENABRAK SANTA");
+                        }
+                    });
+
+                    if (top > max) { overlay("crash", "GAME OVER"); }
+                    if (ticks > 1000) { overlay("cheese", "MAIN PESAWAT ATAU MAIN ROKET???"); }
+
+                    if (santa > 250 - score) {
+                        santa = 0;
+                        $("#plane").before($("<div/>").addClass("santa").css({
+                            "top": (Math.floor(Math.random() * 15) + 1) * 64,
+                            "left": start
+                        }).data("rate", (Math.floor(Math.random() * score / 10) + 2)));
+                    }
+                }, 1);
+            }
+        }
+
+        depressed = true;
+    });
+}
+
+if($(this).mouseup()){
     $(this).mouseup(() => {
         depressed = false;
         ticks = 0;
     });
+}
+if($(this).touchend()){
+    $(this).touchend(() => {
+        depressed = false;
+        ticks = 0;
+    });
+}
+
+
 });
